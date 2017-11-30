891cb35f0d364546f75db78c8ae25313e17fc776
nxshock <nxshock@gmail.com>
2017-11-30 19:07:21 +0500

    fallback to old concurrent map when compiler version <= 1.8

--- userdata_legacy.go
+++ userdata_legacy.go
@@ -1,11 +1,12 @@
-// +build go1.9
+// +build !go1.9
 
 package tox
 
 import (
 	"fmt"
 	"runtime"
-	"sync"
+
+	"github.com/streamrail/concurrent-map"
 )
 
 /*
@@ -16,20 +17,20 @@ import "C"
 
 type userData struct {
 	ud0 map[*C.Tox]*Tox
-	ud1 *sync.Map
+	ud1 cmap.ConcurrentMap
 	cc  bool // concurrent?
 }
 
 func newUserData() *userData {
 	cc := true
 	var ud0 map[*C.Tox]*Tox
-	var ud1 *sync.Map
+	var ud1 cmap.ConcurrentMap
 
 	if runtime.GOMAXPROCS(0) == 1 {
 		cc = false
 		ud0 = make(map[*C.Tox]*Tox, 0)
 	} else {
-		ud1 = new(sync.Map)
+		ud1 = cmap.New()
 	}
 
 	return &userData{ud0: ud0, ud1: ud1, cc: cc}
@@ -38,7 +39,7 @@ func newUserData() *userData {
 func (this *userData) set(ctox *C.Tox, gtox *Tox) {
 	if this.cc {
 		key := this.obj2Str(ctox)
-		this.ud1.Store(key, gtox)
+		this.ud1.Set(key, gtox)
 	} else {
 		this.ud0[ctox] = gtox
 	}
@@ -47,7 +48,7 @@ func (this *userData) set(ctox *C.Tox, gtox *Tox) {
 func (this *userData) get(ctox *C.Tox) *Tox {
 	if this.cc {
 		key := this.obj2Str(ctox)
-		ival, ok := this.ud1.Load(key)
+		ival, ok := this.ud1.Get(key)
 		if !ok {
 			return nil
 		}
@@ -64,7 +65,7 @@ func (this *userData) get(ctox *C.Tox) *Tox {
 func (this *userData) del(ctox *C.Tox) {
 	if this.cc {
 		key := this.obj2Str(ctox)
-		this.ud1.Delete(key)
+		this.ud1.Remove(key)
 	} else {
 		if _, ok := this.ud0[ctox]; ok {
 			delete(this.ud0, ctox)
@@ -78,20 +79,20 @@ func (this *userData) obj2Str(ctox *C.Tox) string {
 
 type userDataAV struct {
 	ud0 map[*C.ToxAV]*ToxAV
-	ud1 *sync.Map
+	ud1 cmap.ConcurrentMap
 	cc  bool // concurrent?
 }
 
 func newUserDataAV() *userDataAV {
 	cc := true
 	var ud0 map[*C.ToxAV]*ToxAV
-	var ud1 *sync.Map
+	var ud1 cmap.ConcurrentMap
 
 	if runtime.GOMAXPROCS(0) == 1 {
 		cc = false
 		ud0 = make(map[*C.ToxAV]*ToxAV, 0)
 	} else {
-		ud1 = new(sync.Map)
+		ud1 = cmap.New()
 	}
 
 	return &userDataAV{ud0: ud0, ud1: ud1, cc: cc}
@@ -100,7 +101,7 @@ func newUserDataAV() *userDataAV {
 func (this *userDataAV) set(ctox *C.ToxAV, gtox *ToxAV) {
 	if this.cc {
 		key := this.obj2Str(ctox)
-		this.ud1.Store(key, gtox)
+		this.ud1.Set(key, gtox)
 	} else {
 		this.ud0[ctox] = gtox
 	}
@@ -109,7 +110,7 @@ func (this *userDataAV) set(ctox *C.ToxAV, gtox *ToxAV) {
 func (this *userDataAV) get(ctox *C.ToxAV) *ToxAV {
 	if this.cc {
 		key := this.obj2Str(ctox)
-		ival, ok := this.ud1.Load(key)
+		ival, ok := this.ud1.Get(key)
 		if !ok {
 			return nil
 		}
@@ -126,7 +127,7 @@ func (this *userDataAV) get(ctox *C.ToxAV) *ToxAV {
 func (this *userDataAV) del(ctox *C.ToxAV) {
 	if this.cc {
 		key := this.obj2Str(ctox)
-		this.ud1.Delete(key)
+		this.ud1.Remove(key)
 	} else {
 		if _, ok := this.ud0[ctox]; ok {
 			delete(this.ud0, ctox)
