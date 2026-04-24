ae5bef323fda11f8b123962150b0ab9d75142203
egitop <egitop@uroad.xyz>
2026-04-24 22:45:29 +0800

    impr send

--- tbcom/packet.go
+++ tbcom/packet.go
@@ -87,9 +87,9 @@ func (p *Packet) NewAckPacket(sid uint64) *Packet{
 
 /////////////////
 
-const ConnTimeo = 5*time.Second
-const WriteTimeo = 5*time.Second
-const ReadTimeo = 5*time.Second
+const ConnTimeo = 10*time.Second
+const WriteTimeo = 20*time.Second
+const ReadTimeo = 30*time.Second
 
 // send rated and timeouted over toxtp
 // global toxnet rate, not for per connection
@@ -102,21 +102,30 @@ const baserate = 10*1024 // 10KB/s
 func NewToxRated(t *tox.Tox) *ToxRated {
 	tra := &ToxRated{}
 	tra.t = t
-	tra.bkt = ratelimit.NewBucketWithRate(5*baserate, 10*baserate)
+	tra.bkt = ratelimit.NewBucketWithRate(12*baserate, 10*baserate)
 	return tra
 }
 
 func (tra *ToxRated) Send(frid uint32, data string) error {
-	// tra.bkt.Take(int64(len(data)))
-	tra.bkt.Wait(int64(len(data))) // = Take + Sleep
-
 	var t = tra.t
 	var err error
 	var retry int
-	for retry = 0; retry < 30; retry ++ {
+
+	// tra.bkt.Take(int64(len(data)))
+	err = t.FriendSendLosslessPacket(frid, data)
+	if err == nil {
+		tra.bkt.Wait(int64(len(data))/2) // = Take + Sleep
+		return nil
+	}
+	tra.bkt.Wait(int64(len(data))) // = Take + Sleep
+
+	// about 20s if all error
+	for retry = 0; retry < 60; retry ++ {
 		err = t.FriendSendLosslessPacket(frid, data)
 		// gopp.ErrPrint(err, frid, "retry")
 		if err == nil { break }
+		if err.Error() == "error: 7" { // sendq full
+		}
 		time.Sleep(333*time.Millisecond)
 	}
 	gopp.ErrPrint(err, frid, "retry", retry)
